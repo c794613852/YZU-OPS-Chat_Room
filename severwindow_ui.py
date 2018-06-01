d2989e53422dc522a0f59c3281eaf079d20874c7
raymondraccoon <gm15gm1515@gmail.com>
2018-06-01 14:26:12 +0800

    severui
    severui update

---
+++ severwindow_ui.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'C:\Users\pc01\Desktop\severwindow.ui'
+#
+# Created by: PyQt5 UI code generator 5.10.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_SeverWindow(object):
+    def setupUi(self, SeverWindow):
+        SeverWindow.setObjectName("SeverWindow")
+        SeverWindow.resize(348, 581)
+        self.centralwidget = QtWidgets.QWidget(SeverWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.nickname_label = QtWidgets.QLabel(self.centralwidget)
+        self.nickname_label.setGeometry(QtCore.QRect(10, 10, 51, 16))
+        self.nickname_label.setLineWidth(1)
+        self.nickname_label.setTextFormat(QtCore.Qt.AutoText)
+        self.nickname_label.setObjectName("nickname_label")
+        self.password_label = QtWidgets.QLabel(self.centralwidget)
+        self.password_label.setGeometry(QtCore.QRect(170, 10, 51, 16))
+        self.password_label.setObjectName("password_label")
+        self.nickname_line = QtWidgets.QLineEdit(self.centralwidget)
+        self.nickname_line.setGeometry(QtCore.QRect(60, 10, 101, 21))
+        self.nickname_line.setObjectName("nickname_line")
+        self.password_line = QtWidgets.QLineEdit(self.centralwidget)
+        self.password_line.setGeometry(QtCore.QRect(220, 10, 111, 20))
+        self.password_line.setInputMask("")
+        self.password_line.setEchoMode(QtWidgets.QLineEdit.Password)
+        self.password_line.setObjectName("password_line")
+        self.textBrowser = QtWidgets.QTextBrowser(self.centralwidget)
+        self.textBrowser.setGeometry(QtCore.QRect(10, 80, 321, 431))
+        self.textBrowser.setObjectName("textBrowser")
+        self.det_button = QtWidgets.QPushButton(self.centralwidget)
+        self.det_button.setGeometry(QtCore.QRect(10, 520, 321, 31))
+        self.det_button.setObjectName("det_button")
+        self.add_button = QtWidgets.QPushButton(self.centralwidget)
+        self.add_button.setGeometry(QtCore.QRect(10, 40, 321, 31))
+        self.add_button.setObjectName("add_button")
+        self.checkBox1 = QtWidgets.QCheckBox(self.centralwidget)
+        self.checkBox1.setEnabled(False)
+        self.checkBox1.setGeometry(QtCore.QRect(20, 90, 301, 16))
+        self.checkBox1.setText("")
+        self.checkBox1.setObjectName("checkBox1")
+        SeverWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(SeverWindow)
+        self.statusbar.setObjectName("statusbar")
+        SeverWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(SeverWindow)
+        QtCore.QMetaObject.connectSlotsByName(SeverWindow)
+
+    def retranslateUi(self, SeverWindow):
+        _translate = QtCore.QCoreApplication.translate
+        SeverWindow.setWindowTitle(_translate("SeverWindow", "Chat Application"))
+        self.nickname_label.setText(_translate("SeverWindow", "NickName"))
+        self.password_label.setText(_translate("SeverWindow", "Password"))
+        self.det_button.setText(_translate("SeverWindow", "Det"))
+        self.add_button.setText(_translate("SeverWindow", "Add"))
+
